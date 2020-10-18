55184daedf89a82543b07f147dd75490cab9cfb1
erastus <erastuskariuki15@gmail.com>
2020-10-18 18:23:22 +0300

    add changes

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -14,132 +14,145 @@ $(document).ready(function () {
   };
 
   Pizza.prototype.getPizzaTypePrice = function () {
+    console.log(
+      this.pizzaSize === "Regular",
+      this.pizzaType === "Bbq",
+      "drama"
+    );
     if (this.pizzaSize === "Regular") {
       if (this.pizzaType === "Bbq") {
-        console.log(this.pizzaType, this.pizzaSize, "func")
         return 350;
-      } else if (this.pizzaType === 'Chicken') {
+      } else if (this.pizzaType === "Chicken") {
         return 500;
-      } else if (this.pizzaType === 'Veggie') {
+      } else if (this.pizzaType === "Veggie") {
         return 300;
-      } else if (this.pizzaType === 'Hawaiian') {
+      } else if (this.pizzaType === "Hawaiian") {
         return 500;
+      } else {
+        return 100;
       }
     } else if (this.pizzaSize === "Medium") {
-      if (this.pizzaType === 'Bbq') {
+      if (this.pizzaType === "Bbq") {
         return 500;
-      } else if (this.pizzaType === 'Chicken') {
+      } else if (this.pizzaType === "Chicken") {
         return 650;
-      } else if (this.pizzaType === 'Veggie') {
+      } else if (this.pizzaType === "Veggie") {
         return 550;
-      } else if (this.pizzaType === 'Hawaiian') {
+      } else if (this.pizzaType === "Hawaiian") {
         return 650;
+      } else {
+        return 100;
       }
     } else if (this.pizzaSize === "Large") {
-      if (this.pizzaType === 'Bbq') {
+      if (this.pizzaType === "Bbq") {
         return 700;
-      } else if (this.pizzaType === 'Chicken') {
+      } else if (this.pizzaType === "Chicken") {
         return 850;
-      } else if (this.pizzaType === 'Veggie') {
+      } else if (this.pizzaType === "Veggie") {
         return 700;
-      } else if (this.pizzaType === 'Hawaiian') {
+      } else if (this.pizzaType === "Hawaiian") {
         return 850;
+      } else {
+        return 100;
       }
     } else if (this.pizzaSize === "Extra-Large") {
-      if (this.pizzaType === 'Bbq') {
+      if (this.pizzaType === "Bbq") {
         return 1000;
-      } else if (this.pizzaType === 'Chicken') {
+      } else if (this.pizzaType === "Chicken") {
         return 1050;
-      } else if (this.pizzaType === 'Veggie') {
+      } else if (this.pizzaType === "Veggie") {
         return 950;
-      } else if (this.pizzaType === 'Hawaiian') {
+      } else if (this.pizzaType === "Hawaiian") {
         return 1050;
+      } else {
+        return 100;
       }
     }
-    console.log(this.pizzaSize, this.pizzaType, "check")
+    console.log(this.pizzaSize, this.pizzaType, "check");
   };
+
   Pizza.prototype.getPizzaToppingsPrice = function () {
     if (this.pizzaSize === "Regular") {
-      if (this.pizzaToppings === 'Bacon') {
+      if (this.pizzaToppings === "Bacon") {
         return 35;
-      } else if (this.pizzaToppings === 'chicken') {
+      } else if (this.pizzaToppings === "chicken") {
         return 50;
-      } else if (this.pizzaToppings === 'Extra-cheese') {
+      } else if (this.pizzaToppings === "Cheese") {
         return 30;
-      } else if (this.pizzaToppings === 'Meat') {
+      } else if (this.pizzaToppings === "Meat") {
         return 50;
       }
     } else if (this.pizzaSize === "Medium") {
-      if (tthis.pizzaToppings === 'Bacon') {
+      if (tthis.pizzaToppings === "Bacon") {
         return 50;
-      } else if (this.pizzaToppings === 'Chicken') {
+      } else if (this.pizzaToppings === "Chicken") {
         return 65;
-      } else if (this.pizzaToppings === 'Extra-cheese') {
+      } else if (this.pizzaToppings === "Cheese") {
         return 55;
-      } else if (this.pizzaToppings === 'Meat') {
+      } else if (this.pizzaToppings === "Meat") {
         return 65;
       }
     } else if (this.pizzaSize === "Large") {
-      if (this.pizzaToppings === 'Bacon') {
+      if (this.pizzaToppings === "Bacon") {
         return 100;
-      } else if (this.pizzaToppings === 'Chicken') {
+      } else if (this.pizzaToppings === "Chicken") {
         return 80;
-      } else if (this.pizzaToppings === 'Extra-cheese') {
+      } else if (this.pizzaToppings === "Cheese") {
         return 70;
-      } else if (this.pizzaToppings === 'Meat') {
+      } else if (this.pizzaToppings === "Meat") {
         return 80;
       }
     } else if (this.pizzaSize === "Extra-Large") {
-      if (this.pizzaToppings === 'Bacon') {
+      if (this.pizzaToppings === "Bacon") {
         return 100;
-      } else if (this.pizzaToppings === 'Chicken') {
+      } else if (this.pizzaToppings === "Chicken") {
         return 105;
-      } else if (this.pizzaToppings === 'Extra-cheese') {
+      } else if (this.pizzaToppings === "Cheese") {
         return 50;
-      } else if (this.pizzaToppings === 'Meat') {
+      } else if (this.pizzaToppings === "Meat") {
         return 100;
       }
     }
   };
   Pizza.prototype.getPizzaCrustWeightprice = function () {
     if (this.pizzaSize === "Regular") {
-        if (this.crustWeight === 'Thin') {
-          return 50;
-        } else if (this.crustWeight === 'Thick') {
-          return 100;
-        } else if (this.crustWeight === 'Double') {
-          return 150;
-        } else if (this.crustWeight === 'Crispy') {
-          return 100;
-        }
-      }else if (this.pizzaSize === "Medium") {
-        if (this.crustWeight === 'Thin') {
-          return 50;
-        } else if (this.crustWeight === 'Thick') {
-          return 100;
-        } else if (this.crustWeight === 'Double') {
-          return 150;
-        } else if (this.crustWeight === 'Crispy') {
-          return 100;
-        }
-      }else if (this.pizzaSize === "Large") {
-      if (this.crustWeight === 'Thin') {
+      if (this.crustWeight === "Thin") {
         return 50;
-      } else if (this.crustWeight === 'Thick') {
+      } else if (this.crustWeight === "Thick") {
         return 100;
-      } else if (this.crustWeight === 'Double') {
+      } else if (this.crustWeight === "Double") {
         return 150;
-      } else if (this.crustWeight === 'Crispy') {
+      } else if (this.crustWeight === "Crispy") {
+        return 100;
+      }
+    } else if (this.pizzaSize === "Medium") {
+      if (this.crustWeight === "Thin") {
+        return 50;
+      } else if (this.crustWeight === "Thick") {
+        return 100;
+      } else if (this.crustWeight === "Double") {
+        return 150;
+      } else if (this.crustWeight === "Crispy") {
+        return 100;
+      }
+    } else if (this.pizzaSize === "Large") {
+      if (this.crustWeight === "Thin") {
+        return 50;
+      } else if (this.crustWeight === "Thick") {
+        return 100;
+      } else if (this.crustWeight === "Double") {
+        return 150;
+      } else if (this.crustWeight === "Crispy") {
         return 100;
       }
     } else if (this.pizzaSize === "Extra.Large") {
-      if (this.crustWeight === 'Thin') {
+      if (this.crustWeight === "Thin") {
         return 50;
-      } else if (this.crustWeight === 'Thick') {
+      } else if (this.crustWeight === "Thick") {
         return 100;
-      } else if (this.crustWeight === 'Double') {
+      } else if (this.crustWeight === "Double") {
         return 150;
-      } else if (this.crustWeight === 'Crispy') {
+      } else if (this.crustWeight === "Crispy") {
         return 100;
       }
     }
@@ -155,15 +168,21 @@ $(document).ready(function () {
 
   $("#myForm").submit(function (event) {
     event.preventDefault();
-    console.log("click", $("#pizzaType").val())
+    console.log(
+      "click",
+      $("#pizzaType").val(),
+      $("#pizzaSize").val(),
+      $("#pizzaToppings").val(),
+      $("#crustWeight").val()
+    );
     var pizzaTypeSelected = $("#pizzaType").val();
     var pizzaSizeSelected = $("#pizzaSize").val();
     var pizzaToppingsSelected = $("#pizzaToppings").val();
     var crustWeightSelected = $("#crustWeight").val();
 
     var newPizza = new Pizza(
-      pizzaTypeSelected,
       pizzaSizeSelected,
+      pizzaTypeSelected,
       pizzaToppingsSelected,
       crustWeightSelected
     );
@@ -175,38 +194,37 @@ $(document).ready(function () {
 
     totalCost = 0;
 
-
     for (let i = 0; i < pizzasOrdered.length; i++) {
       totalCost += pizzasOrdered[i].getPizzaPrice();
     }
-    console.log(newPizza)
-    console.log(newPizza.pizzaPrice)
-    console.log(newPizza.getPizzaTypePrice(), "")
+    console.log(newPizza);
+    console.log(newPizza.pizzaPrice);
+    console.log(newPizza.getPizzaTypePrice(), "crims");
 
     $("#bill-summary").append(
       "<tr>" +
-      '<th scope="row">' +
-      newPizza.pizzaType +
-      " -" +
-      newPizza.getPizzaTypePrice() +
-      "</th>" +
-      "<td>" +
-      newPizza.pizzaToppings +
-      " - " +
-      newPizza.getPizzaToppingsPrice() +
-      "</td>" +
-      "<td>" +
-      newPizza.crustWeight +
-      " - " +
-      newPizza.getPizzaCrustWeightprice() +
-      "</td>" +
-      "<td>" +
-      newPizza.pizzaSize +
-      "</td>" +
-      "<td>" +
-      newPizza.getPizzaPrice() +
-      "</td>" +
-      "</tr>"
+        '<th scope="row">' +
+        newPizza.pizzaType +
+        " -" +
+        newPizza.getPizzaTypePrice() +
+        "</th>" +
+        "<td>" +
+        newPizza.pizzaToppings +
+        " - " +
+        newPizza.getPizzaToppingsPrice() +
+        "</td>" +
+        "<td>" +
+        newPizza.crustWeight +
+        " - " +
+        newPizza.getPizzaCrustWeightprice() +
+        "</td>" +
+        "<td>" +
+        newPizza.pizzaSize +
+        "</td>" +
+        "<td>" +
+        newPizza.getPizzaPrice() +
+        "</td>" +
+        "</tr>"
     );
     if (pizzasOrdered.length > 0) {
       $(".form-control").empty();
@@ -234,36 +252,51 @@ $(document).ready(function () {
   //   console.log(name);
   //   console.log(deliveryOption);
 
-    $("#checkout-form").submit(function (event) {
+  $("#checkout-form").submit(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var deliveryOption = $("#delivery-option").val();
+    customerName = name;
+    console.log(name);
+    console.log(deliveryOption);
+    $("#name").val("");
+    $("#delivery-option").val("");
+    $("#checkout-options").hide();
+    if (deliveryOption === "deliver") {
+      $("#street").show();
+      $(".delivery-cost").show();
+      $("#delivery-amount").append(200);
+      totalCost += 200;
+      $("#total-amount").empty();
+      $("#total-amount").append(totalCost);
+    } else {
+      alert(
+        customerName +
+          ": Your total bill is Ksh. " +
+          totalCost +
+          ". Your order will be ready for collection in the next 30mins"
+      );
+    }
+    $("#delivery-form").submit(function (event) {
       event.preventDefault();
-      var name = $("#name").val();
-      var deliveryOption = $("#delivery-option").val();
-      customerName = name;
-      console.log(name);
-      console.log(deliveryOption);
-      $("#name").val("");
-      $("#delivery-option").val("");
-      $("#checkout-options").hide();
-      if (deliveryOption === "deliver") {
-        $("#street").show();
-        $(".delivery-cost").show();
-        $("#delivery-amount").append(200);
-        totalCost += 200;
-        $("#total-amount").empty();
-        $("#total-amount").append(totalCost);
-      } else {
-        alert(customerName + ": Your total bill is Ksh. " + totalCost + ". Your order will be ready for collection in the next 30mins");
-      }
-      $("#delivery-form").submit(function (event) {
-        event.preventDefault();
-        var streetEntered = $("#street").val();
-        var countyEntered = $("#county").val();
-        var contactNumberEntered = $("#number").val();
-        street = streetEntered;
-        county = countyEntered;
-        contactNumber = contactNumberEntered;
-        $("#county").hide();
-        alert(customerName + ": Your total bill is Ksh. " + totalCost + ". Your order will be delivered to " + street + ", " + contactNumber + " in the next 30mins");
-      });
+      var streetEntered = $("#street").val();
+      var countyEntered = $("#county").val();
+      var contactNumberEntered = $("#number").val();
+      street = streetEntered;
+      county = countyEntered;
+      contactNumber = contactNumberEntered;
+      // $("#county").hide();
+    
+      alert(
+        customerName +
+          ": Your total bill is Ksh. " +
+          totalCost +
+          ". Your order will be delivered to " +
+          street +
+          ", " +
+          contactNumber +
+          " in the next 30mins"
+      );
     });
   });
+});
